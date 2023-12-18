b8542a8d0032e9b5921f915f8d492cd0225a5212
parveensuyan <parveensuyan@gmail.com>
2023-12-18 10:35:45 -0800

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,11 @@
+<?php 
+//starting of php
+
+
+echo "Hello World!";
+
+// echo is used to the print the statment string ['' or ""]
+// "" for the string
+// ; is to end the statement
+?> 
+<!-- closing tag -->
