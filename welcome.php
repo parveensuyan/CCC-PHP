26f220d92ba4bfe604416cbfc510b14260753f9f
parveensuyan <parveensuyan@gmail.com>
2023-12-19 08:41:04 -0800

    Create welcome.php

---
+++ welcome.php
@@ -0,0 +1,20 @@
+<?php 
+// variable to store some piece of information
+// assignment operator  =   (equal to)
+// variable should have $ sign in order to assign/store any value
+
+$name  = 'Parveen';
+$student_name = 'ABC';
+$studentnamefromcoursea = 'xyz';
+$studentNameFromCourseA = 'xyz';
+// Naming convention  :  you should not use -(hyphen),space,special characters 
+
+                        //  you can add _ (underscore) ,should start with alphabet
+
+// camel case  : where every word first letter should be caps
+echo "Instructor name is ".$name;
+echo "<br>";
+echo "Student Name is ".$student_name;
+
+// .(dot) is join two string together 
+?>
