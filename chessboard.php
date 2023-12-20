a5a864b8164bdb8423dcac4372831f6f7153917a
parveensuyan <parveensuyan@gmail.com>
2023-12-20 09:31:49 -0800

    Add files via upload

--- chessboard.php
+++ chessboard.php
@@ -7,8 +7,11 @@ another for row
 first for($i=1;$i=10;$i++)
 second for($j=1;$j=5;$j++) 
 */
+// iteration/Loop : repeat the code until the condition is not false
 
-for($i=1;$i<4;$i++){
+for($i=1;$i<4;$i++){ 
+
+// until condition will not be false its going to repeat j (inner loop) again and again
   for($j=1;$j<4;$j++) {
       echo $i.' '.$j."<br>";
 
@@ -33,6 +36,19 @@ j = 1
  2 3
 j = 3+1;4<4 condition will become false and return to first loop
 
+i = 3
+j = 1 ;1<4
+
+3 1
+j= 1+1 = 2<4
+3 2
+j= 2+1 = 3<4
+3 3
+j= 3+1 = 4<4
+ i = 3
+//  i=  3+1 = 4<4 condition become false
+
+
 1 1
 1 2
 1 3
@@ -45,6 +61,7 @@ j = 3+1;4<4 condition will become false and return to first loop
 */
   }
 }
+  echo "returned from nested loop";
 
 
 ?>
