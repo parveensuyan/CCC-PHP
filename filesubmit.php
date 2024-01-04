f096c01b2d5e02d7848828cd5d4ffcd30e58b8f1
parveensuyan <parveensuyan@gmail.com>
2024-01-04 07:47:25 -0800

    Add files via upload

--- filesubmit.php
+++ filesubmit.php
@@ -1,4 +1,15 @@
 <?php
 
-print_r($_FILES);
+$targetDir ="./images";
+  if(is_array($_FILES)) {
+    if(is_uploaded_file($_FILES['image']['tmp_name'])) {
+        $destinationPath  =  $targetDir."/".$_FILES["image"]["name"];
+        if(move_uploaded_file($_FILES['image']['tmp_name'],$destinationPath)) {
+        // move the file from temp location to the destination folder
+        // 1 . temppath,    2. target/destination path
+        echo "<img src = $destinationPath width = 200px> ";
+        }
+}
+}
+
 ?>
